a9e79c772189c74285fd1971f3da401a19e73891
gabo52281 <gabo52281@gmail.com>
2025-10-17 15:09:03 -0500

    nuevo link axios backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://backend-sistema-jet.vercel.app/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
